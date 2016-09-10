f52c6f172b8201ab784e3174487cbf6069582710
Haruko Okada <haruko.flipsy.krb@gmail.com>
2016-09-10 17:57:00 +0900

    hello

--- js/application.js
+++ js/application.js
@@ -3,18 +3,20 @@ window.onload = function () {
     var now = new Date();
     var day_fes = new Date(2016, 8, 17);
     var delta = (day_fes - now) / 86400000;
-
-    if (delta > -1 && delta <= 0) {
+    if(label != null) {
+      if (delta > -1 && delta <= 0) {
         label.textContent = "学園祭初日!";
-    } else if (delta > -2 && delta <= -1) {
-        label.textContent = "学園祭2日目!";
-    } else {
-        label.textContent = "残り" + Math.floor(delta) + "日";
+      } else if (delta > -2 && delta <= -1) {
+          label.textContent = "学園祭2日目!";
+      } else {
+          label.textContent = "残り" + Math.floor(delta) + "日";
+      }
     }
+    
     test();
 };
 
 function test() {
-    $(".a").fadeToggle(3000);
-    setTimeout("test()", 3000);
+    $(".a").fadeToggle(5000);
+    setTimeout("test()", 5000);
 }
